4fbfaa6b1da8ff7d9081a8a45d1cbcca0f09624e
Avishkar2004 <avishkarkakde2004@gmail.com>
2024-02-11 14:14:24 +0530

    New Update

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
-const mysql2 = require("mysql2");
 const app = express();
 const PORT = 8000;
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const { db } = require("./connection");
 
 app.use(cors());
+app.use(bodyParser.json());
 
+//this is a middleware or plugin
+app.use(express.urlencoded({ extended: false }));
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
@@ -39,6 +42,7 @@ app.get("/insecticide", (req, res) => {
   );
 });
 
+//gettind
 app.get("/cart", (req, res) => {
   db.query(
     "SELECT id, name, price, image FROM cart",
@@ -65,6 +69,43 @@ app.get("/cart", (req, res) => {
       res.json(updatedResults);
     }
   );
+});
+
+// For inserting data
+app.post("/cart/:id", (req, res) => {
+  try {
+    const newItem = req.body;
+    console.log("Received request to add to cart:", newItem);
+    // Convert base64 image to buffer
+    const binaryImage = newItem.image
+      ? Buffer.from(newItem.image, "base64")
+      : null;
+
+    const insertOrUpdateQuery = `
+      INSERT INTO cart (id, name, price, image)
+      VALUES (?, ?, ?, ?)
+      ON DUPLICATE KEY UPDATE
+      name = VALUES(name),
+      price = VALUES(price),
+      image = VALUES(image)
+      `;
+    db.query(
+      insertOrUpdateQuery,
+      [newItem.id, newItem.name, newItem.price, binaryImage],
+      (err, results) => {
+        if (err) {
+          console.error("Error inserting or updating cart item:", err);
+          return res
+            .status(500)
+            .json({ error: "Internal server error in Cart" });
+        }
+        res.json({ ...newItem });
+      }
+    );
+  } catch (error) {
+    console.error("Unexpected error in /cart route:", error);
+    res.status(500).json({ error: "Internal server error in Cart" });
+  }
 });
 
 app.listen(PORT, () => {
