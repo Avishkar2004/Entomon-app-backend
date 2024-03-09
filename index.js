c5d35036ae1c5612f44045f88aaa614827d2523e
Avishkar2004 <avishkarkakde2004@gmail.com>
2024-03-09 12:09:32 +0530

    Cluster

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const status = require("express-status-monitor");
 const compression = require("compression");
-const startCluster = require("./cluster");
+const startCluster = require("./Cluster/cluster");
 
 const {
   getProductData,
